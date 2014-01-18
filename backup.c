829d5130d6603da8357fd77dff954a71f5308132
Tim Vincent <tim.vincent@outlook.com>
2014-01-18 02:29:04 -0500

    Update backup.c

--- backup.c
+++ backup.c
@@ -4,12 +4,13 @@
 #define secondsToHours 3600;
 /**
 * Backup creates a scheduled backup for the set cycle (in hours)
-*
+* @author Tim Vincent
 **/
 int main(int argc, char *argv[])
 {
 	int cycle = atoi(argv);
 	
+	//Protect from bad or no input
 	if(cycle < 1)
 		n = secondsToHours;
 	n = n * secondsToHours;
