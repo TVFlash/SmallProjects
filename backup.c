ff9845ee286eb99acdc975e69489c1425c527fa9
root <root@master.flashswords.com>
2014-01-18 07:27:29 +0000

    Scheduled Linux backup

---
+++ backup.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<time.h>
+
+#define secondsToHours 3600;
+/**
+* Backup creates a scheduled backup for the set cycle (in hours)
+*
+**/
+int main(int argc, char *argv[])
+{
+	int cycle = atoi(argv);
+	
+	if(cycle < 1)
+		n = secondsToHours;
+	n = n * secondsToHours;
+
+	while(1 == 1)
+	{ 	
+		sleep(cycle);
+		runBackup();
+	}
+}
+
+
+int runBackup()
+{
+	system("echo Creating a backup...");
+	system("zip -R Backup.zip ./* ");
+	system("echo Done!");
+}
