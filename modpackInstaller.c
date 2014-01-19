b02acedfd59bdf90f7d0406d42cd7e194e2313fd
Tim Vincent <tim.vincent@outlook.com>
2014-01-19 00:01:23 -0500

    Create modpackInstaller.c

---
+++ modpackInstaller.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stddef.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+
+        FILE *infile;
+        FILE *outfile;
+        int pos = 0, ch;
+        char c;
+        char fileName[64], usr[32], chown[32];
+
+        strcpy(fileName, "unzip ");
+        strcat(fileName, argv[1]);
+        system(fileName);
+
+        //Create a temp file and past the user ID into it
+        outfile = fopen("temp.txt", "w");
+        system("stat -c %U server.log > temp.txt");
+        fclose(outfile);
+
+        //Open temp file and retreive ID
+        infile = fopen("temp.txt", "r");
+        int ret = fscanf(infile, "%s %c",usr,&c);
+        fclose(infile);
+        system("rm temp");
+
+        //Queue up the chown
+        strcpy(chown, "chown -R ");
+        strcat(chown, usr);
+        strcat(chown, ":");
+        strcat(chown, usr);
+        strcat(chown, " *");
+        system(chown);
+
+        //Patch user file perms
+        system("chmod -R a+r+x *");
+
+        //Mission success
+        return 0;
+}
+
+
